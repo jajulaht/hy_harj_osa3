7e337f3bff545a0cef7bd7f0a7946139fa520545
Janne Lahtinen <janne.lahtinen@grafinote.fi>
2019-09-20 11:12:24 +0300

    puhelinluettelon backend step 2 done

--- index.js
+++ index.js
@@ -2,34 +2,45 @@ const express = require('express')
 const app = express()
 
 let persons = [
-  {
-    "persons":[
-      { 
-        "name": "Arto Hellas", 
-        "number": "040-123456",
-        "id": 1
-      },
-      { 
-        "name": "Ada Lovelace", 
-        "number": "39-44-5323523",
-        "id": 2
-      },
-      { 
-        "name": "Dan Abramov", 
-        "number": "12-43-234345",
-        "id": 3
-      },
-      { 
-        "name": "Mary Poppendieck", 
-        "number": "39-23-6423122",
-        "id": 4
-      }
-    ]
+  { 
+    "name": "Arto Hellas", 
+    "number": "040-123456",
+    "id": 1
+  },
+  { 
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523",
+    "id": 2
+  },
+  { 
+    "name": "Dan Abramov", 
+    "number": "12-43-234345",
+    "id": 3
+  },
+  { 
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122",
+    "id": 4
+  },
+  { 
+    "name": "Risto Reipas", 
+    "number": "050-6423122",
+    "id": 5
   }
 ]
 
+// Root
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
+})
+
+// Info
+app.get('/info', (req, res) => {
+  let info = persons.length
+  let now = new Date()
+  res.send(`<p>Phone book has info for ${info} people</p>
+            <p>${now}</p>`
+          )
 })
 
 app.get('/api/persons', (req, res) => {
