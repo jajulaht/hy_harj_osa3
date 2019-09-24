18ba773f16847a67ec55538b94c5cb80d8c14ccd
Janne Lahtinen <janne.lahtinen@grafinote.fi>
2019-09-24 15:41:53 +0300

    puhelinluettelon backend fullstack done

---
+++ build/precache-manifest.04acf9aa86338816f79843f54992a9ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92c12fb4685905fe8c509dd4e31a8a74",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b53c766311c9d8800b5b",
+    "url": "/static/css/main.6c64fd65.chunk.css"
+  },
+  {
+    "revision": "fc47316148352cf70402",
+    "url": "/static/js/2.44f0b47d.chunk.js"
+  },
+  {
+    "revision": "b53c766311c9d8800b5b",
+    "url": "/static/js/main.8d4117bb.chunk.js"
+  },
+  {
+    "revision": "235accea7dab4cb774d7",
+    "url": "/static/js/runtime~main.88d66cb1.js"
+  }
+]);
---
+++ build/precache-manifest.cb04b89b44aff88c954db8952788b088.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cccea4bacb1a8ee91fc8c59ec5be2e1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab48265128f2edf7bdd2",
+    "url": "/static/css/main.6c64fd65.chunk.css"
+  },
+  {
+    "revision": "fc47316148352cf70402",
+    "url": "/static/js/2.44f0b47d.chunk.js"
+  },
+  {
+    "revision": "ab48265128f2edf7bdd2",
+    "url": "/static/js/main.bda35504.chunk.js"
+  },
+  {
+    "revision": "235accea7dab4cb774d7",
+    "url": "/static/js/runtime~main.88d66cb1.js"
+  }
+]);
